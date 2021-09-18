4e45ff4ced48f54efd8608c66756a2eae807034a
Samuel Desmond Kurtzer <Email@SamuelKurtzer.com>
2021-09-18 22:16:41 +0930

    updating urls files

--- resume/urls.py
+++ resume/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
         path('', views.index, name="index"),
-]
+] + static('/file', document_root='pdf/ResumeSamuelKurtzer201904.pdf')
 
